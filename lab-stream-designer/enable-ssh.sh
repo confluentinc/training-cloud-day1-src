904c727710bb742932cb103ae724093a920709ca
Borja Hernandez Crespo <borja.hernandez.crespo@gmail.com>
2023-07-07 13:10:23 +0200

    Merge branch 'master' of https://github.com/confluentinc/training-cloud-day1-src

--- lab-stream-designer/enable-ssh.sh
+++ lab-stream-designer/enable-ssh.sh
@@ -32,7 +32,7 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 echo "Done!"
 
 # Use curl to get the public IP address
-public_ip=$(curl  -s https://api.ipify.org)
+public_ip=$(curl  -s https://api64.ipify.org)
 
 echo " "
 echo "PUBLIC IP ADDRESS: $public_ip"
